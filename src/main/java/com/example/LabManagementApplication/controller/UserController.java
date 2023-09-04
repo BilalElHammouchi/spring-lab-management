9246f004d324982a6524b7c337f16b12aa14d3f7
GoldenDovah <bilal-mok@hotmail.fr>
2023-09-04 20:29:24 +0100

    fixed security access to index

--- src/main/java/com/example/LabManagementApplication/controller/UserController.java
+++ src/main/java/com/example/LabManagementApplication/controller/UserController.java
@@ -3,11 +3,14 @@ package com.example.LabManagementApplication.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.example.LabManagementApplication.model.Users;
 import com.example.LabManagementApplication.service.UserService;
@@ -23,9 +26,9 @@ public class UserController {
     }
 
     @RequestMapping(method=RequestMethod.POST, value="/create-user")
-    public ResponseEntity<Users> createUser(@RequestBody UserRequest userRequest) {
+    public RedirectView createUser(@RequestBody UserRequest userRequest) {
         System.out.println("Received JSON data: " + userRequest.toString());
-        Users newUser = userService.createUser(
+        userService.createUser(
             userRequest.getFirstName(),
             userRequest.getLastName(),
             userRequest.getEmail(),
@@ -33,6 +36,11 @@ public class UserController {
             userRequest.getPassword()
         );
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+            userRequest.getEmail(),
+            userRequest.getPassword()
+        );
+
+        return new RedirectView("/index");
     }
 }
