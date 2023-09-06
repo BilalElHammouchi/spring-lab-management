5d775a862701733c86977bfa79deb6869a119fc1
TheKillerBill <bilal-mok@hotmail.fr>
2023-09-07 00:09:52 +0100

    added automatic login after sign up

--- src/main/java/com/example/LabManagementApplication/controller/UserController.java
+++ src/main/java/com/example/LabManagementApplication/controller/UserController.java
@@ -1,28 +1,29 @@
 package com.example.LabManagementApplication.controller;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
+
 import com.example.LabManagementApplication.service.UserService;
 import com.example.LabManagementApplication.web.UserRequest;
 
 @RestController
 public class UserController {
-    private final UserService userService;
 
+    private final UserService userService;
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
     @RequestMapping(method=RequestMethod.POST, value="/create-user")
-    public RedirectView createUser(@RequestBody UserRequest userRequest) {
-        System.out.println("Received JSON data: " + userRequest.toString());
+    public UserRequest createUser(@RequestBody UserRequest userRequest, HttpServletRequest request) {
         userService.createUser(
             userRequest.getFirstName(),
             userRequest.getLastName(),
@@ -30,12 +31,15 @@ public class UserController {
             userRequest.getRole(),
             userRequest.getPassword()
         );
-
-        Authentication authentication = new UsernamePasswordAuthenticationToken(
-            userRequest.getEmail(),
-            userRequest.getPassword()
-        );
-
-        return new RedirectView("/index");
+        authWithHttpServletRequest(request, userRequest.getEmail(), userRequest.getPassword());
+        return userRequest;
     }
+
+    public void authWithHttpServletRequest(HttpServletRequest request, String username, String password) {
+        try {
+            request.login(username, password);
+        } catch (ServletException e) {
+        }
+    }
+
 }
