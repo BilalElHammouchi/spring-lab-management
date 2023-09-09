f3e4ea37db9e15e514b1af4d0049edf3a329f0dd
TheKillerBill <bilal-mok@hotmail.fr>
2023-09-10 00:00:28 +0100

    replaced post request with thymeleaf

--- src/main/java/com/example/LabManagementApplication/controller/UserController.java
+++ src/main/java/com/example/LabManagementApplication/controller/UserController.java
@@ -4,18 +4,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.view.RedirectView;
-
-import com.example.LabManagementApplication.model.Users;
 import com.example.LabManagementApplication.service.UserService;
 import com.example.LabManagementApplication.web.UserRequest;
 
@@ -46,13 +38,6 @@ public class UserController {
             request.login(username, password);
         } catch (ServletException e) {
         }
-    }
-
-    @GetMapping("/current-user")
-    public UserDetails getCurrentUser() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        return userDetails;
     }
 
 }
