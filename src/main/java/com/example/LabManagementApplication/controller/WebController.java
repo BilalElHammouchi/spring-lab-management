696c2726a96b7cafe704aaa39b0397cc8e048daa
GoldenDovah <bilal-mok@hotmail.fr>
2023-09-10 00:00:28 +0100

    replaced post request with thymeleaf

--- src/main/java/com/example/LabManagementApplication/controller/WebController.java
+++ src/main/java/com/example/LabManagementApplication/controller/WebController.java
@@ -1,14 +1,31 @@
 package com.example.LabManagementApplication.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.LabManagementApplication.model.Users;
+import com.example.LabManagementApplication.repository.UserRepository;
 
 
 @Controller
 public class WebController {
 
+    @Autowired
+    UserRepository userRepository;
+
     @GetMapping("/index")
-    public String index() {
+    public String index(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        Users user = userRepository.findbyEmail(username);
+        model.addAttribute("first_name", user.getFirstName());
+        model.addAttribute("last_name", user.getLastName());
+        model.addAttribute("email", user.getEmail());
+        model.addAttribute("role", user.getRole());
         return "index";
     }
 
--- src/main/java/com/example/LabManagementApplication/security/LabSecurity.java
+++ src/main/java/com/example/LabManagementApplication/security/LabSecurity.java
@@ -2,13 +2,11 @@ package com.example.LabManagementApplication.security;
 
 import java.util.Arrays;
 
-import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -24,7 +22,6 @@ import com.example.LabManagementApplication.service.CustomUserDetailsService;
 public class LabSecurity extends WebSecurityConfigurerAdapter {
 
   @Autowired
-  private DataSource dataSource;
   
   @Bean
   public CustomUserDetailsService userDetailsService() {
